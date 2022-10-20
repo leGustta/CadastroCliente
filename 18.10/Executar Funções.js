b736a7fe94d0d3c9b0a2c49907f04fffdeed8a59
leGustta <100322240+leGustta@users.noreply.github.com>
2022-10-20 10:54:58 -0400

    Add files via upload
    20-10

---
+++ 18.10/Executar Funções.js
@@ -0,0 +1,40 @@
+
+    function multiplica(num1,num2) {
+        let resultado = num1 * num2;
+        return resultado;
+    }
+
+    function divide(num1,num2) {
+        let resultado = num1 / num2;
+        return resultado;
+    }
+
+    function soma(num1,num2) {
+        let resultado = num1 + num2;
+        return resultado;
+    }
+
+    function subtração(num1,num2) {
+        let resultado = num1 - num2;
+        return resultado;
+    }
+
+    function IMC (peso,altura) {
+        let resultado = peso / (altura * altura);
+        return resultado;
+    }
+
+    function desconto(num1,num2) {
+        let resultado = num1 - num2;
+        return resultado;
+    }
+
+    function Salario (num1) {
+        let resultado = num1 + (num1*0.08);
+        return resultado;
+    }
+
+    function idade(num1) {
+        let resultado = (2022-num1);
+        return resultado;
+    }
